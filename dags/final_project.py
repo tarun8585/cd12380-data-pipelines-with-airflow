15a5bd05560a612095923d81096cf3880f3ac534
arunkumar thirunavukarasu <athiru2@wgu.edu>
2026-02-07 17:01:53 -0500

    Removed Credentials before submission

--- dags/final_project.py
+++ dags/final_project.py
@@ -17,7 +17,7 @@ default_args = {
 @dag(
     default_args=default_args,
     description='Load and transform data in Redshift with Airflow',
-    schedule_interval='0 * * * *',
+    schedule_interval='0 * * * *', # Dag scheduled hourly
     max_active_runs=1
 )
 
--- set_connections_and_variables.sh
+++ set_connections_and_variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-airflow connections add aws_credentials --conn-uri 'aws://AKIA264YYGFTLOL6E5AL:MI%2FaL9wr2fou7qwJDojWq8MeHmCUjbxhpDElxMmi@'
-airflow connections add redshift --conn-uri 'redshift://awsuser:R3dsh1ft@default-workgroup.753550242150.us-east-1.redshift-serverless.amazonaws.com:5439/dev'
-airflow variables set s3_bucket arun-thiru
-airflow variables set s3_prefix data-pipelines
+airflow connections add aws_credentials --conn-uri 'aws://***************'
+airflow connections add redshift --conn-uri 'redshift://*****************:5439/dev'
+airflow variables set s3_bucket udacity-dend
+airflow variables set s3_prefix log_data
